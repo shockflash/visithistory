164a984a8921a466497fa0af5b4586ebcf714095
LH <shockflash@web.de>
2011-09-02 10:41:57 +0200

    Fixed testapp name in settings.py of testapp

--- testapp/settings.py
+++ testapp/settings.py
@@ -109,7 +109,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'testapp2.urls'
+ROOT_URLCONF = 'testapp.urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
